f90314690bd59a5293f81c1a4552642aace3ca1d
lbw0319 <liubaowei0319@gmail.com>
2022-07-24 00:36:50 +0900

    test commit order

---
+++ src/App1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(value,index) in arr" :key=index>{{ value }}</li>
+    </ul>
+    <input type="button" value="走一走" @click="turn">
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      arr:['帅哥','美女','程序员','野兽']
+    }
+  },
+  methods:{
+    turn(){
+      this.arr.push(this.arr[0]);
+      this.arr.shift();
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/App2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(value,index) in myarr" :key = index>
+        {{ value }}
+        <input type="button" value="删除" @click="del(index)">
+        </li>
+    </ul>
+    <input type="button" value="生成" @click="add">
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      myarr:[1,2,3]
+    }
+  },
+  methods:{
+    add(){
+      let num = Math.round(Math.random()*1000);
+      this.myarr.push(num);
+    },
+    del(index){
+      this.myarr.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
